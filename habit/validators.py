83f9b264965e71eacadffc4d4bbcce740e0a2030
Sergey <smirsand@mail.ru>
2023-11-08 18:25:03 +0300

    add code validators.py habit

--- habit/validators.py
+++ habit/validators.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework.exceptions import ValidationError
 
 
@@ -5,6 +7,7 @@ class ExceptionValidator:
     """
     Валидатор исключения одновременного выбора полей связанная привычка и награда.
     """
+
     def __init__(self, related_habit, reward):
         self.related_habit = related_habit
         self.reward = reward
@@ -15,3 +18,18 @@ class ExceptionValidator:
 
         if related_habit and reward:
             raise ValidationError("Вы не можете выбрать одновременно связанную привычку и награду.")
+
+
+class TimeValidator:
+    """
+    Валидатор времени выполнения.
+    """
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        time = value.get(self.field)
+        if time > datetime.time(minute=2):
+            raise ValidationError('Время должно быть не больше 120 секунд')
+
