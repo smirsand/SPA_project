1ea46051fe91c827f025895a1eba3ac874d62ae9
Sergey <smirsand@mail.ru>
2023-11-09 14:52:28 +0300

    add code urls.py habit

--- habit/urls.py
+++ habit/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from habit.apps import HabitConfig
-from habit.views import HabitCreateAPIView
+from habit.views import HabitCreateAPIView, HabitListAPIView
 
 app_name = HabitConfig.name
 
 urlpatterns = [
-                  path('habit/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habit/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habit/list/', HabitListAPIView.as_view(), name='habit-list'),
 
-              ]
+]
