b6e59158a89c27ce5c3cba8e08fd414081dd6558
Sergey <smirsand@mail.ru>
2023-11-09 14:53:05 +0300

    add code views.py habit

--- habit/views.py
+++ habit/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 
+from habit.models import Habit
+from habit.paginators import HabitPaginator
 from habit.serializers import HabitSerializer
 
 
@@ -8,3 +10,12 @@ class HabitCreateAPIView(generics.CreateAPIView):
     Контроллер создания привычки.
     """
     serializer_class = HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """
+    Контроллер просмотра списка привычек.
+    """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    pagination_class = HabitPaginator
