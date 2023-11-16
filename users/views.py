e99670dffad9e72d1ff6b561ece6c2baf6da814b
Sergey <smirsand@mail.ru>
2023-11-16 18:00:26 +0300

    add code views.py users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from users.serializers import UserSerializer
+from users.models import User
+from users.serializers import UserSerializer, MyTokenObtainPairSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
@@ -8,3 +10,8 @@ class UserCreateAPIView(generics.CreateAPIView):
     Контроллер создания пользователя.
     """
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
